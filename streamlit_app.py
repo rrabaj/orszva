bda59dd653eb14e3ca9441e0993b2712cf1e811f
rrabaj <66421217+rrabaj@users.noreply.github.com>
2025-02-09 12:11:10 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,26 @@ import sqlite3
 
 conn = sqlite3.connect("adatok.db")
 c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS users (nev TEXT, eletkor INTEGER)")
+c.execute("CREATE TABLE IF NOT EXISTS users (nev TEXT, betu TEXT, orszag TEXT, varos TEXT, fiu TEXT, lany TEXT, noveny TEXT, allat TEXT, hires TEXT,hegy TEXT, viz TEXT,targy TEXT)")
 conn.commit()
 
-st.title("Adatfeltöltő táblázat")
+st.title("Ország város játék")
 
 nev = st.text_input("Név")
-eletkor = st.number_input("Életkor", min_value=0, max_value=120, step=1)
+betu = st.text_input("Betű")
+orszag = st.text_input("Ország")
+varos = st.text_input("Város")
+fiu = st.text_input("Fiú")
+lany = st.text_input("Lány")
+noveny = st.text_input("Növény")
+allat = st.text_input("Állat")
+hires = st.text_input("Híres ember")
+hegy = st.text_input("Hegy")
+viz = st.text_input("Víz")
+targy = st.text_input("Térgy")
 
 if st.button("Mentés"):
-    c.execute("INSERT INTO users (nev, eletkor) VALUES (?, ?)", (nev, eletkor))
+    c.execute("INSERT INTO users (nev, betu , orszag , varos , fiu , lany , noveny , allat , hires ,hegy , viz ,targy) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,)", (nev, betu , orszag , varos , fiu , lany , noveny , allat , hires ,hegy , viz ,targy))
     conn.commit()
     st.success("Sikeresen mentve!")
 
